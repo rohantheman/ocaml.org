3797afb6b508e8befbe66b19c843c6e710eb794c
Rohan Henry <rohan.henry@gmail.com>
2017-04-29 13:24:34 -0400

    completed proof of concept for parsing json code

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
-gem 'net/http'
-gem 'json'
-
+require 'net/http'
+require 'json'
+require 'uri'
+require 'date'
 
 url = 'http://healthycanadians.gc.ca/recall-alert-rappel-avis/api/recent/en'
 uri = URI(url)
 response = Net::HTTP.get(uri)
 
-if response == '200'
-    result = JSON.parse(response)
-    puts result
-else
-    puts 'ERROR!!!'
+recalls = JSON.parse(response)
+
+# puts recalls['results']['ALL']
+
+recalls['results']['ALL'].each do |doc|
+  puts 'Recall ID: ' + doc['recallId']
+  puts 'Recall Title: ' + doc['title']
+  puts 'Category: ' + doc['category'].to_s
+  puts 'Date Published: ' + Time.at(doc['date_published']).to_datetime.to_s
+  puts 'URL: ' + doc['url']
+  puts '**************'
+  puts ''
 end
