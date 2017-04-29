4125480d12b9a551cac56b1a2d72de33cff6ac73
Rohan Henry <rohan.henry@gmail.com>
2017-04-29 13:33:45 -0400

    created  code in block

--- app.rb
+++ app.rb
@@ -6,17 +6,17 @@ require 'date'
 url = 'http://healthycanadians.gc.ca/recall-alert-rappel-avis/api/recent/en'
 uri = URI(url)
 response = Net::HTTP.get(uri)
-
 recalls = JSON.parse(response)
 
-# puts recalls['results']['ALL']
-
-recalls['results']['ALL'].each do |doc|
-  puts 'Recall ID: ' + doc['recallId']
-  puts 'Recall Title: ' + doc['title']
-  puts 'Category: ' + doc['category'].to_s
-  puts 'Date Published: ' + Time.at(doc['date_published']).to_datetime.to_s
-  puts 'URL: ' + doc['url']
-  puts '**************'
-  puts ''
-end
+show_results = lambda {
+  recalls['results']['ALL'].each do |doc|
+    puts 'Recall ID: ' + doc['recallId']
+    puts 'Recall Title: ' + doc['title']
+    puts 'Category: ' + doc['category'].to_s
+    puts 'Date Published: ' + Time.at(doc['date_published']).to_datetime.to_s
+    puts 'URL: ' + doc['url']
+    puts '**************'
+    puts ''
+  end
+}
+show_results.call
